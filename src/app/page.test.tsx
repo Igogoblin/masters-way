7ad45a8cb559a47565573b576fb8a841aca036b9
Ihar Skavysh <igogoblin@gmail.com>
2024-12-16 20:51:40 +0300

    feat: add storybook and vitest

---
+++ src/app/page.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import Home from "./page";
+
+describe("page", () => {
+  it("renders the Next.js logo", () => {
+    render(<Home />);
+    const logo = screen.getByAltText("Next.js logo");
+    expect(logo).toBeInTheDocument();
+  });
+
+  it("renders the list items", () => {
+    render(<Home />);
+    const listItems = screen.getAllByRole("listitem");
+    expect(listItems).toHaveLength(2);
+    expect(screen.getByText("Проверка деплоя")).toBeInTheDocument();
+    expect(screen.getByText("Деплой на vercel.")).toBeInTheDocument();
+  });
+});
